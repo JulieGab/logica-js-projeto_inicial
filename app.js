db993101e0b3e1ee3527664a36d07fda8b9e9d6e
JulieGab <jullygabriella.sa@gmail.com>
2023-11-30 12:13:04 -0300

    feat: operador ternário e número aleatório

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 alert('Bem vindo ao jogo do número secreto!');
-
-let numeroSecreto = 5;
+let numeroSecreto = parseInt(Math.random() * 10 + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
@@ -8,10 +7,10 @@ let tentativas = 1;
 
 // enquanto o chute não for igual ao n.s:
 while(chute != numeroSecreto){
-    chute = prompt('Escolha um número entre 1 e 10')
-    // se chute for igual ao número secreto
+    chute = prompt('Escolha um número entre 1 e 100')
+    // se chute for igual ao número secretojgbgiy
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+        break;
     } else {
         if (chute > numeroSecreto){
                 alert(`O número secreto é menor que ${chute}`);
@@ -23,3 +22,11 @@ while(chute != numeroSecreto){
     }
 }
 
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+// if (tentativas == 1) {
+//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+// } else {
+//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+// }
